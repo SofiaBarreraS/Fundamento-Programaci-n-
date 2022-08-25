0cb4ccb5b9085ef443d9b4d95f9ad2b63950f892
SofiaBarreraS <>
2022-08-25 17:40:00 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//Hola mundo 
+//Sofia Barrera Sanchez 
+//25-08-2022
+/* version 1 */
+
+//Bibliotecas 
+#include <stdio.h>
+
+//Funcion principal 
+int main(void) {
+
+  //Funcion de impresion en pantalla 
+  printf("Hola Mundo\n");
+  return 0;
+}
